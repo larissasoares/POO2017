c9516ca76bfffc336b0c4a2c3ba2b32bbe85b813
IFSP <IFSP@lab3pc14>
2017-08-22 21:36:31 -0300

    Alteração do projeto no Netbeans.

--- aula5/src/aula5/Aula5.java
+++ aula5/src/aula5/Aula5.java
@@ -17,6 +17,8 @@ public class Aula5 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println("Hello world!");
+        
         
     }
     
